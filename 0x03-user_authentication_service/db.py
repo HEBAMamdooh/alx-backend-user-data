bd0ddb48c2f6913d76c6b28eef2be63d98af805a
abdallh <heba.mamdooh975@gmail.com>
2024-11-23 14:06:07 +0200

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,12 @@ DB module
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from sqlalchemy.orm.session import Session
 from user import Base, User
 
+
+VALID_FIELDS = {"id", "email", "hashed_password", "session_id", "reset_token"}
 
 class DB:
     """DB class to interact with the database
@@ -41,7 +44,34 @@ class DB:
         Returns:
             User: The newly created User object.
         """
+        if not email or not hashed_password:
+            return        
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
+        # Explicitly query and return the user to ensure correct response
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        find_user_by.
+        """
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        update_user.
+        """
+        session = self._session
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in VALID_FIELDS:
+                raise ValueError
+            setattr(user, k, v)
+        session.commit()
